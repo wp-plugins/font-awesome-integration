ad7944ebc75b1f9bc773522a227bd7cabb0f9eb8
mcostales84 <mcostales84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-18 00:12:05 +0000

    Taging version 1.1.
    git-svn-id: https://plugins.svn.wordpress.org/font-awesome-integration/trunk@1092933 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- font-awesome-integration.php
+++ font-awesome-integration.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Plugin Name: Font Awesome Integration
+ * Plugin URI: https://wordpress.org/plugins/font-awesome-integration
  * Description: This plugin integrate the Font Awesome library with your wordpress installation.
  * Version: 1.1
  * Author: Manuel Costales
