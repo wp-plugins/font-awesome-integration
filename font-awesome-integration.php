af13dc7b046ef99421c15fc2e689742b6113773d
mcostales84 <mcostales84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-27 23:51:07 +0000

    creating tag 2.0
    git-svn-id: https://plugins.svn.wordpress.org/font-awesome-integration/trunk@1122540 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- font-awesome-integration.php
+++ font-awesome-integration.php
@@ -3,7 +3,7 @@
  * Plugin Name: Font Awesome Integration
  * Plugin URI: http://www.jumptoweb.com
  * Description: This plugin integrate the Font Awesome library with your wordpress installation.
- * Version: 1.1
+ * Version: 2.0
  * Author: Manuel Costales
  * Author URI: http://www.manuelcostales.com
  */
@@ -12,4 +12,36 @@ defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 add_action('wp_head', 'add_fawesome_mc');
 function add_fawesome_mc() {
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
+}
+
+
+add_shortcode( 'fawesome', 'fawesome_shortcode_mc' );
+function fawesome_shortcode_mc( $atts ) {
+/*
+ * Attributes availables
+ * aclass -> classes to use in the <a> tag
+ * target -> target of the <a> tag (_blank, _self, _parent, _top)
+ * href   -> link to use in the <a> tag
+ * iclass -> classes to use in the <i> tag
+ *
+ * All this attributes but the iclass are optionals
+ */
+	if (is_array($atts) && $atts['iclass']){ 
+		$iclass = $atts['iclass'];}
+	else {
+		$iclass = "fa-flag";}
+
+	if (is_array($atts) && $atts['aclass']){ 
+		$aclass = $atts['aclass'];}
+
+	if (is_array($atts) && $atts['target']){ 
+		$target = $atts['target'];}
+
+	if (is_array($atts) && $atts['href']){ 
+		$href = $atts['href'];}
+
+	return isset($href) ? 
+		"<a class=' ".$aclass."' href='".$href."' target=' ".$target."'><i class='fa ".$iclass."'></i></a>"
+		: 
+		"<i class='fa ".$iclass."'></i>";
 }
